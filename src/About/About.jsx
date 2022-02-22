c807c085174c457d2ce5c1687057eb8a87a4fab0
ScottR <astroplexity@gmail.com>
2022-02-21 22:50:14 -0500

    first commit

---
+++ src/About/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+//import "./About.css"
+
+const About = () => {
+  return (
+    <div className="about">
+      Hey my name is Scott, Welcome to my portfolio
+    </div>
+  )
+}
+
+export default About
---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import About from '../About/About';
+
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h2 id="about">A little about me :D!! </h2>
+        <About />
+      </div>
+    </div>
+  );
+}
+
+export default App;

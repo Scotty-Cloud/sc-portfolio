ff38093333ad2b93a61d20ec65da579c7900716a
ScottR <astroplexity@gmail.com>
2022-02-24 23:35:11 -0500

    fix project links

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -5,14 +5,32 @@ const Projects = () => {
   return (
     <div className="myProjects">
       <div className="pokemonProj">
-      <a href="https://pokemonunown.netlify.app/" target="_blank" rel="noreferrer">Pokemon Memory Game</a>
+      <a href="https://pokemonunown.netlify.app/" target="_blank" rel="noreferrer">Unseen Forces</a>
+      <br />
+      Pokemon Memory Card Flip Game
       <br />
       <a href="https://github.com/Scotty-Cloud/Pokemon-Memory-Game" target="_blank" rel="noreferrer">Github</a>
       </div>
       <div className="HammyProj">
-      <a href="https://pokemonunown.netlify.app/" target="_blank" rel="noreferrer">Pokemon Memory Game</a>
+      <a href="https://wanna-hammy-out.herokuapp.com/" target="_blank" rel="noreferrer">Wanna Hammy Out</a>
       <br />
-      <a href="https://github.com/Scotty-Cloud/Pokemon-Memory-Game" target="_blank" rel="noreferrer">Github</a>
+      Its Hammy Time
+      <br />
+      <a href="https://github.com/Scotty-Cloud/wanna-hammy-out" target="_blank" rel="noreferrer">Github</a>
+      </div>
+      <div className="elvisProj">
+      <a href="https://graceland-movies.herokuapp.com/" target="_blank" rel="noreferrer">Graceland</a>
+      <br />
+      Just another heart break hotel story
+      <br />
+      <a href="https://github.com/Scotty-Cloud/pinkcadillac" target="_blank" rel="noreferrer">Github</a>
+      </div>
+      <div className="lfgProj">
+      <a href="https://graceland-movies.herokuapp.com/" target="_blank" rel="noreferrer">Graceland</a>
+      <br />
+      Just another heart break hotel story
+      <br />
+      <a href="https://github.com/Scotty-Cloud/pinkcadillac" target="_blank" rel="noreferrer">Github</a>
       </div>
     </div>
   )

51ec067dcd928fee96b183044c1e3f9d70895817
ScottR <astroplexity@gmail.com>
2022-02-24 23:19:35 -0500

    add second project/fix proj class

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -4,9 +4,16 @@ import React from "react";
 const Projects = () => {
   return (
     <div className="myProjects">
+      <div className="pokemonProj">
       <a href="https://pokemonunown.netlify.app/" target="_blank" rel="noreferrer">Pokemon Memory Game</a>
       <br />
       <a href="https://github.com/Scotty-Cloud/Pokemon-Memory-Game" target="_blank" rel="noreferrer">Github</a>
+      </div>
+      <div className="HammyProj">
+      <a href="https://pokemonunown.netlify.app/" target="_blank" rel="noreferrer">Pokemon Memory Game</a>
+      <br />
+      <a href="https://github.com/Scotty-Cloud/Pokemon-Memory-Game" target="_blank" rel="noreferrer">Github</a>
+      </div>
     </div>
   )
 }

1051fa35586293ed6513321fc7e9010e1d57719e
ScottR <astroplexity@gmail.com>
2022-02-27 23:46:43 -0500

    start nav

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Nav from '../Nav/Nav'
 import About from '../About/About';
 import Projects from '../Projects/Projects';
 import Contact from '../Contact/Contact'

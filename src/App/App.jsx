3f76ff8eddbfb62fca6e426a09756f9957955b22
scotty <devscottycloud@gmail.com>
2022-03-29 15:18:15 -0400

    working on parallax effect

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,38 +2,57 @@ import React from 'react';
 import './App.css';
 // import Nav from '../Nav/Nav'
 import About from '../About/About';
-import Projects from '../Projects/Projects';
-import Contact from '../Contact/Contact'
-import Footer from '../Footer/Footer'
+// import Projects from '../Projects/Projects';
+// import Contact from '../Contact/Contact'
+// import Footer from '../Footer/Footer'
 
-const App = () => {
+// const App = () => {
  
-  return (
+//   return (
     
       
-      <div>
+//       <div className="myBackGround">
        
-        <div className='abt abg'>
-          <h2 id="about">A little about me :D!! </h2>
+//         <div className='aboutMe'>
+//           <h2 id="about">A little about me :D!! </h2>
+//         <About />
+//       </div>
+
+//       <div>
+//         <h2 id="project"> View My Projects Ive completed</h2>
+//         <Projects />
+//       </div>
+
+//       <div>
+//         <h2 id="contact"> Contact Me</h2>
+//         <Contact />
+//       </div>
+
+//       <div>
+//         <Footer />
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const App = () => {
+
+  return(
+
+    <div>
+
+      <div class="main-bg about-bg">
+        <h2> Who am I, But just another face in the wind</h2>
         <About />
       </div>
 
-      <div>
-        <h2 id="project"> View My Projects Ive completed</h2>
-        <Projects />
-      </div>
-
-      <div>
-        <h2 id="contact"> Contact Me</h2>
-        <Contact />
-      </div>
-
-      <div>
-        <Footer />
-      </div>
 
     </div>
-  );
+
+  )
 }
 
 export default App;
